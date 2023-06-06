aba97f8c038f9c3361fc2f98712817ccfbf2dd66
Franksml <fmedinal@unal.edu.co>
2023-06-06 14:16:16 -0500

    before of keras

--- striwey/yoloV8.py
+++ striwey/yoloV8.py
@@ -21,8 +21,6 @@ class ObjectDetection:
         
         self.CLASS_NAMES_DICT = self.model.model.names
     
-        self.box_annotator = BoxAnnotator(color=ColorPalette(), thickness=3, text_thickness=3, text_scale=1.5)
-    
 
     def load_model(self):
        
@@ -39,54 +37,58 @@ class ObjectDetection:
         return results
     
 
-    def plot_bboxes(self, results, frame):
-        counter = 0
-        xyxys = []
-        confidences = []
-        class_ids = []
-        
-        # Extract detections for person class
-        for result in results[0]:
-            class_id = result.boxes.cls.cpu().numpy().astype(int)
-            
-            if class_id == 0:
-                
-                xyxys.append(result.boxes.xyxy.cpu().numpy())
-                confidences.append(result.boxes.conf.cpu().numpy())
-                class_ids.append(result.boxes.cls.cpu().numpy().astype(int))
-            
-        if counter <= 100:
-            print(result.boxes.xyxy.cpu().numpy())
-            counter += 1
+    def detecting_person_and_ball(self, results, frame):
+        person, ball, val = False
 
-
-        # Setup detections for visualization
         detections = Detections(
                     xyxy=results[0].boxes.xyxy.cpu().numpy(),
                     confidence=results[0].boxes.conf.cpu().numpy(),
                     class_id=results[0].boxes.cls.cpu().numpy().astype(int),
                     )
+        for i, (xyxy, confidence, class_id, tracker_id) in enumerate(detections):
+            if class_id == 0:
+                person = True
+            if class_id == 1:
+                ball = True
         
+        if person and ball:
+            val = True
 
-        # Format custom labels
-        self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
-        for _, confidence, class_id, tracker_id
-        in detections]
-        
-        # Annotate and display frame
-        frame = self.box_annotator.annotate(frame=frame, detections=detections, labels=self.labels)
-        
-        return frame
+        return val
     
+
+    def draw_flow(self, img, flow, step=20):
+
+        h, w = img.shape[:2]
+        y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+        fx, fy = flow[y,x].T
+
+        lines = np.vstack([x, y, x-fx, y-fy]).T.reshape(-1, 2, 2)
+        lines = np.int32(lines + 0.5)
+
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        cv2.polylines(img_bgr, lines, 0, (0, 255, 0))
+
+        for (x1, y1), (_x2, _y2) in lines:
+            cv2.circle(img_bgr, (x1, y1), 1, (0, 255, 0), -1)
+
+        return img_bgr
     
     
     def __call__(self):
-
-        cap = cv2.VideoCapture(self.capture_index)
+        
+        
+        videoPath = 'dom2.mp4'
+        cap = cv2.VideoCapture('./TrainingVideos/' + videoPath)
         assert cap.isOpened()
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-      
+
+        width  = int(cap.get(3))
+        height = int(cap.get(4))
+        #output = cv2.VideoWriter('./Striwey/Output/output_' + videoPath, cv2.VideoWriter_fourcc(*'mp4v'), 50, (width, height))
+
+        suc, prev = cap.read()
+        prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+
         while True:
           
             start_time = time()
@@ -94,8 +96,18 @@ class ObjectDetection:
             ret, frame = cap.read()
             assert ret
             
+            #Yolo v8
             results = self.predict(frame)
-            frame = self.plot_bboxes(results, frame)
+            valBool = self.detecting_person_and_ball(results, frame)
+
+            if valBool:
+                #opticalFlow
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+                prevgray = gray
+                frame = self.draw_flow(gray, flow)
+            else:
+                print('No se encuentra la persona y el balón')
             
             end_time = time()
             fps = 1/np.round(end_time - start_time, 2)
@@ -103,10 +115,12 @@ class ObjectDetection:
             cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
             
             cv2.imshow('YOLOv8 Detection', frame)
+            #output.write(frame)
  
             if cv2.waitKey(5) & 0xFF == 27:
                 break
         
+        #output.release()
         cap.release()
         cv2.destroyAllWindows()
         
